2e62493626e81f941f41555d053d35557e79025e
lullabyX <hassan_rabbi@live.com>
2026-03-12 08:11:12 +0600

    fix: adjust MediaGrid breakpoints to 1-6 columns

--- src/components/MediaGrid.tsx
+++ src/components/MediaGrid.tsx
@@ -8,7 +8,7 @@ interface MediaGridProps {
 export default function MediaGrid({ children }: MediaGridProps) {
   return (
     <div className="@container">
-      <div className="grid grid-cols-2 @lg:grid-cols-3 @3xl:grid-cols-4 @5xl:grid-cols-5 gap-5">
+      <div className="grid grid-cols-1 @sm:grid-cols-2 @lg:grid-cols-3 @3xl:grid-cols-4 @5xl:grid-cols-5 @7xl:grid-cols-6 gap-5">
         {children}
       </div>
     </div>
